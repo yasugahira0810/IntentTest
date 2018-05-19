ea3fd3eb2eb41256ceb98b17780142d849b49680
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-05-19 23:14:08 +0900

    Implement recieving message

--- app/src/main/java/com/example/intenttest/SubActivity.java
+++ app/src/main/java/com/example/intenttest/SubActivity.java
@@ -1,13 +1,19 @@
 package com.example.intenttest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("message");
+        Toast.makeText(SubActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
