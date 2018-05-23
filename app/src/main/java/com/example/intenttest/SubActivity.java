00a451442623d0d84bd0bf1aae1be1429ea281dd
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-05-23 21:12:18 +0900

    Implement output debug log.

--- app/src/main/java/com/example/intenttest/SubActivity.java
+++ app/src/main/java/com/example/intenttest/SubActivity.java
@@ -4,13 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SubActivity extends AppCompatActivity {
+
+    static final String TAG = "SubActivity";
+
+    private void myLog(String tag, String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        Log.d(tag, msg);
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+
+        myLog(TAG, "***** onCreate *****");
 
         Intent intent = getIntent();
         String message = intent.getStringExtra("message");
--- memo.md
+++ memo.md
@@ -5,3 +5,16 @@
 - インテントを送る時はデータの型気にしなくていいけど、受ける時は気にしてメソッドを変えないといけない。getIntExtraとかgetShortArrayExtraとか。
 - getStringExtra等のインテントからデータを取り出すメソッドがnullを返す可能性がある場合、必ずnullチェックをしてNullPointerExceptionを避ける。
 - onCreateなどのメソッドは、必ず実行されるものとされないものがある。onCreate(), onStart(), onResume(), onPause()は基本的に必ず実行されるが、onStop(), onDestroy()は必ず実行されるわけではない。なのでデータの保存などはonPause()でやる。
+- TOASTはmakeText()がオブジェクトを作成し、show()が表示する。
+- AndroidではリソースのXML内で「android:id=」という指定で要素に名前を付けると、自動的に「R.java」というint型の変数が自動的に作成される。
+- setOnClickListenerメソッドでOnClickListenerを登録して、その中でonClickを定義する。
+
+```java
+        bt_toSub2.setOnClickListener(new View.OnClickListener() {
+```
+
+- EditTextの内容を、ボタン押下時に取得するためには、onClickの中でgetText().toString()のようにして取得する。
+
+```java
+                String message = et_message.getText().toString();
+```
