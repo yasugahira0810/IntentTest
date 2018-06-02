f406219ce36ea895ff4fa524706d99f6c04721f2
Yuta Yasugahira <hourou_freak@yahoo.co.jp>
2018-06-02 21:59:18 +0900

    Implement Exec by Enter

--- app/src/main/java/com/example/intenttest/MainActivity.java
+++ app/src/main/java/com/example/intenttest/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.intenttest;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,24 +52,51 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        EditText et_message = (EditText) findViewById(R.id.editText_message);
+        final String message = et_message.getText().toString();
+
+        et_message.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                //EnterKeyが押されたかを判定
+                if (event.getAction() == KeyEvent.ACTION_DOWN
+                        && keyCode == KeyEvent.KEYCODE_ENTER) {
+
+                    //ソフトキーボードを閉じる
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                    //検索処理
+                    executeSearch(message, packageName);
+
+                }
+                return false;
+            }
+        });
+
         Button bt_toSub2 = (Button)findViewById(R.id.button_toSub2);
         bt_toSub2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                EditText et_message = (EditText) findViewById(R.id.editText_message);
-                String message = et_message.getText().toString();
-                if (message.length() >= 2) {
-                    intent.setClassName(packageName, packageName + ".SubActivity");
-                    intent.putExtra("message", message);
-                    startActivity(intent);
-                } else {
-                    intent.setClassName(packageName, packageName + ".LoadActivity");
-                    startActivityForResult(intent, 0);
-                }
+                executeSearch(message, packageName);
             }
+
         });
     }
+
+    private void executeSearch(String message, String packageName) {
+        Intent intent = new Intent();
+        if (message.length() >= 2) {
+            intent.setClassName(packageName, packageName + ".SubActivity");
+            intent.putExtra("message", message);
+            startActivity(intent);
+        } else {
+            intent.setClassName(packageName, packageName + ".LoadActivity");
+            startActivityForResult(intent, 0);
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
